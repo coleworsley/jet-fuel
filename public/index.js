64eb06b33b5d68776de3949d9f139c563f5cf42a
coleworsley <cole.s.worsley@gmail.com>
2017-08-15 22:25:21 -0600

    add new folder on submit

--- public/index.js
+++ public/index.js
@@ -10,9 +10,28 @@ const appendFolders = (folders) => {
   $('.folder-container').append(folders.map(folder => (
     `<button class="folder" value=${folder.id}>${folder.name}</button>`
   )))
+
+  $('.folder-dropdown').append(folders.map(folder => (
+    `<option>${folder.name}</option>`
+  )))
 }
 
+//  Event Listeners
+$('#new-folder-submit').on('click', (e) => {
+  e.preventDefault();
+  const name = $('#new-folder-input').val();
 
+  fetch('api/v1/folders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name })
+  })
+  .then(res => res.json())
+  .then(id => console.log(id))
+  .catch(error => console.log(error))
+})
 
 $('#url-submit').on('click', (e) => {
   e.preventDefault();
