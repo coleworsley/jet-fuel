2d9eb26d75bf7fc3b9a6181eb094d054c873902a
coleworsley <cole.s.worsley@gmail.com>
2017-08-18 11:29:53 -0600

    configure seeds & circle ci

--- db/seeds/test/testSeeds.js
+++ db/seeds/test/testSeeds.js
@@ -1,42 +1,51 @@
 const folders = [
   {
+    id: 1,
     name: 'Concerts',
   },
   {
+    id: 2,
     name: 'Documentation',
   },
   {
+    id: 3,
     name: 'Social',
   },
 ]
 
 const links = [
   {
+    id: 1,
     original_url: 'http://www.ticketmaster.com/section/concerts',
     short_url: 'placeholder',
     folder_id: 1,
   },
   {
+    id: 2,
     original_url: 'http://www.westword.com/concerts',
     short_url: 'placeholder',
     folder_id: 1,
   },
   {
+    id: 3,
     original_url: 'https://developer.mozilla.org/en-US/',
     short_url: 'placeholder',
     folder_id: 2,
   },
   {
+    id: 4,
     original_url: 'https://expressjs.com/en/guide/routing.html',
     short_url: 'placeholder',
     folder_id: 2,
   },
   {
+    id: 5,
     original_url: 'http://knexjs.org/',
     short_url: 'placeholder',
     folder_id: 2,
   },
   {
+    id: 6,
     original_url: 'https://www.facebook.com/',
     short_url: 'placeholder',
     folder_id: 3,
--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/test/seeds'
+      directory: './db/seeds/test'
     }
   },
 
